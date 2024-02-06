55488031e2a5639459aff0d3de5f0d957b73e059
Igor Chelbogashev <Chelbogashevi@gmail.com>
2024-02-06 06:27:21 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+
+TOKEN = "6366080094:AAE67KpYyJRG4GFa8cR9ZTiEkzl_YqIBsps"
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    Ответ на стартовую команду
+    """
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Обработчик перенаправит полученное сообщение обратно отправителю
+    По умолчанию обработчик сообщений обрабатывает все типы сообщений (текст, фотографию, стикер т.д.)
+    """
+    try:
+        # Отправит копию полученного сообщения
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # отработка ошибок
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Инициализация экземпляра бота с режимом анализа по умолчанию, который будет передаваться всем вызовам API
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
